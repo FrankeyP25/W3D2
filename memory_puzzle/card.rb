5ae25826cbaa0c59aa577b05a0549a279068e6a3
Susan Zea <susanzea16@gmail.com>
2021-10-19 13:37:55 -0400

    updated

---
+++ memory_puzzle/card.rb
@@ -0,0 +1,42 @@
+
+
+
+class Card
+
+    attr_reader :face_value
+
+    def initialize(face_value)
+        @face_value = face_value
+        @face_up = false
+    end
+ 
+
+    def face_up?
+       @face_up 
+    end
+
+    #card_inst.hide
+    def hide
+        @face_up = false
+    end
+
+    def reveal
+        @face_up = true
+    end
+
+
+    # def to_s
+
+    # end
+
+
+    # def ==(second_card_inst)
+
+    # end
+
+
+
+
+
+
+end
